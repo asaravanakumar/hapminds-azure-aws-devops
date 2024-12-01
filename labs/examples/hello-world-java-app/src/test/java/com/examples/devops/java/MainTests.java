e1182111cafdcf4968d46c40b4286cee401ea772
Devops Training <saravana.devopstraining2023@gmail.com>
2024-12-01 13:42:11 +0530

    hello world java app

---
+++ labs/examples/hello-world-java-app/src/test/java/com/examples/devops/java/MainTests.java
@@ -0,0 +1,13 @@
+package com.examples.devops.java;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTests {
+
+    @Test
+    public void testMain() {
+        // Forces to fail the test case
+//        Assertions.assertEquals(1,2);
+    }
+}
